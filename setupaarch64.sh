efc7fab1d861e633ac87c3fb9d251529ba09171f
Kevin <36828226+neo250376@users.noreply.github.com>
2023-02-28 20:10:37 +0000

    Update setupaarch64.sh

--- setupaarch64.sh
+++ setupaarch64.sh
@@ -15,17 +15,21 @@ ANSWER=N
 
 while [[ $ANSWER != "Y" && $ANSWER != "y" && $ANSWER != "" ]]
 do
+        echo -e "\n"
         echo -e -n "Is this ${GREEN} ($WALLET) ${NC} correct? (y)"
         read ANSWER
 
         if [[ $ANSWER != "Y" && $ANSWER != "y" && $ANSWER != "" ]]
         then
-               echo -e -n "${GREEN}Enter your Dero wallet address: ${NC}"
+        echo -e "\n"
+        echo -e -n "${GREEN}Enter your Dero wallet address: ${NC}"
                 read WALLET
         fi
 done
 
-echo -e -n "Using Wallet Address: $WALLET"
+        echo -e "\n"
+
+        echo -e -n "Using Wallet Address: ${GREEN} $WALLET ${NC}"
 
 NODE=dero-node-overlode.mysrv.cloud:$MINER_PORT
 
@@ -33,12 +37,13 @@ ANSWER=N
 
 while [[ $ANSWER != "Y" && $ANSWER != "y" && $ANSWER != "" ]]
 do
-        echo -e -n "Use Following Mining Node? ($NODE)? (y)"
+        echo -e "\n"
+        echo -e -n "Use Following Mining Node? ${GREEN} ($NODE) ${NC}? (y)"
         read ANSWER
 
         if [[ $ANSWER != "Y" && $ANSWER != "y" && $ANSWER != "" ]]
         then
-            echo -e -n "Enter Your Mining Node Address: "
+            echo -n "Enter Your Mining Node Address: "
             read NODE
         fi
 done
@@ -54,7 +59,9 @@ wget -c -q https://github.com/dero-am/astrobwt-miner/releases/download/V1.7-BETA
 
 clear
 
-echo -e "Starting Astrominer using wallet:${GREEN} $WALLET....  ${NC}"
+echo -e -n "Starting Astrominer using wallet:${GREEN} $WALLET  ${NC}"
+
+echo -e "\n"
 
 # Run Miner
 
